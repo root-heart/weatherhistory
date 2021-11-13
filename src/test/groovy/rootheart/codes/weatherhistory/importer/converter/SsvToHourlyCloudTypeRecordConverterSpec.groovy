b1b35b3d995919a02a253647a17b25f7d04fdda5
kai <kontakt@kaidrahmann.de>
2021-11-13 23:09:50 +0100

    fix test for cloud type record converter

--- src/test/groovy/rootheart/codes/weatherhistory/importer/converter/SsvToHourlyCloudTypeRecordConverterSpec.groovy
+++ src/test/groovy/rootheart/codes/weatherhistory/importer/converter/SsvToHourlyCloudTypeRecordConverterSpec.groovy
@@ -5,7 +5,6 @@ import rootheart.codes.weatherhistory.importer.MeasurementOrObservation
 import rootheart.codes.weatherhistory.importer.QualityLevel
 import rootheart.codes.weatherhistory.importer.SsvData
 import rootheart.codes.weatherhistory.importer.StationId
-import rootheart.codes.weatherhistory.importer.converter.SsvToHourlyCloudTypeRecordConverter
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -20,7 +19,6 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
                                 "V_S3_CS", "V_S3_CSA", "V_S3_HHS", "V_S3_NS",
                                 "V_S4_CS", "V_S4_CSA", "V_S4_HHS", "V_S4_NS"]
 
-    static final String NULL_STRING = "-999"
 
     @Unroll('#description')
     def 'Test that strings are converted correctly to hourly cloud type record'() {
@@ -34,7 +32,7 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         def records = converter.convert(ssvData).collect(Collectors.toList())
 
         then: 'each input line has a corresponding cloud type record'
-        records*.stationId == values.collect { StationId.of(it[0]) }
+        records*.stationId == values.collect { it[0] != null ? StationId.of(it[0]) : null }
         records*.measurementTime == values.collect { LocalDateTime.parse(it[1], DateTimeFormatter.ofPattern('yyyyMMddHH')) }
         records*.qualityLevel == values.collect { QualityLevel.of(it[2]) }
         records*.overallCoverage == values.collect { Integer.parseInt(it[3]) }
@@ -47,9 +45,8 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         records.every { it.layer4 == null }
 
         where:
-        description          | values
-        'all fields set'     | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'station id not set' | [['-999', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
+        description      | values
+        'all fields set' | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]]
     }
 
     @Unroll('#description')
@@ -63,14 +60,7 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         when: 'the input is converted'
         def records = converter.convert(ssvData).collect(Collectors.toList())
 
-        then: 'each input line has a corresponding cloud type record'
-        records*.stationId == values.collect { StationId.of(it[0]) }
-        records*.measurementTime == values.collect { LocalDateTime.parse(it[1], DateTimeFormatter.ofPattern('yyyyMMddHH')) }
-        records*.qualityLevel == values.collect { QualityLevel.of(it[2]) }
-        records*.overallCoverage == values.collect { Integer.parseInt(it[3]) }
-        records*.measurementOrObservation == values.collect { MeasurementOrObservation.of(it[4]) }
-
-        and: 'only the first layer is set'
+        then: 'only the first layer is set'
         records.every { it.layer1 != null }
         records.every { it.layer2 == null }
         records.every { it.layer3 == null }
@@ -78,15 +68,15 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
 
         and: 'the values in the first layer are set correctly'
         records*.layer1.collect { it.cloudType } == values.collect { cloudTypeOfCodeAndAbbreviation(it[5], it[6]) }
-        records*.layer1.collect { it.height } == values.collect { it[7] == '-999' ? null : Integer.parseInt(it[7]) }
-        records*.layer1.collect { it.coverage } == values.collect { it[8] == '-999' ? null : Integer.parseInt(it[8]) }
+        records*.layer1.collect { it.height } == values.collect { it[7] == null ? null : Integer.parseInt(it[7]) }
+        records*.layer1.collect { it.coverage } == values.collect { it[8] == null ? null : Integer.parseInt(it[8]) }
 
         where:
         description                                     | values
-        'one record, cloud type code on layer1'         | [['44', '1979010100', '1', '2', 'P', '1', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud type abbreviation on layer1' | [['44', '1979010100', '1', '2', 'P', '-999', 'CI', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud height on layer1'            | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '1234', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, coverage on layer1'                | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '5', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
+        'one record, cloud type code on layer1'         | [['44', '1979010100', '1', '2', 'P', '1', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]]
+        'one record, cloud type abbreviation on layer1' | [['44', '1979010100', '1', '2', 'P', null, 'CI', null, null, null, null, null, null, null, null, null, null, null, null, null, null]]
+        'one record, cloud height on layer1'            | [['44', '1979010100', '1', '2', 'P', null, null, '1234', null, null, null, null, null, null, null, null, null, null, null, null, null]]
+        'one record, coverage on layer1'                | [['44', '1979010100', '1', '2', 'P', null, null, null, '5', null, null, null, null, null, null, null, null, null, null, null, null]]
     }
 
     @Unroll('#description')
@@ -100,14 +90,7 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         when: 'the input is converted'
         def records = converter.convert(ssvData).collect(Collectors.toList())
 
-        then: 'each input line has a corresponding cloud type record'
-        records*.stationId == values.collect { StationId.of(it[0]) }
-        records*.measurementTime == values.collect { LocalDateTime.parse(it[1], DateTimeFormatter.ofPattern('yyyyMMddHH')) }
-        records*.qualityLevel == values.collect { QualityLevel.of(it[2]) }
-        records*.overallCoverage == values.collect { Integer.parseInt(it[3]) }
-        records*.measurementOrObservation == values.collect { MeasurementOrObservation.of(it[4]) }
-
-        and: 'only the second layer is set'
+        then: 'only the second layer is set'
         records.every { it.layer1 == null }
         records.every { it.layer2 != null }
         records.every { it.layer3 == null }
@@ -115,15 +98,15 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
 
         and: 'the values in the second layer are set correctly'
         records*.layer2.collect { it.cloudType } == values.collect { cloudTypeOfCodeAndAbbreviation(it[9], it[10]) }
-        records*.layer2.collect { it.height } == values.collect { it[11] == '-999' ? null : Integer.parseInt(it[11]) }
-        records*.layer2.collect { it.coverage } == values.collect { it[12] == '-999' ? null : Integer.parseInt(it[12]) }
+        records*.layer2.collect { it.height } == values.collect { it[11] == null ? null : Integer.parseInt(it[11]) }
+        records*.layer2.collect { it.coverage } == values.collect { it[12] == null ? null : Integer.parseInt(it[12]) }
 
         where:
         description                                     | values
-        'one record, cloud type code on layer2'         | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '1', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud type abbreviation on layer2' | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', 'CI', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud height on layer2'            | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '1234', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, coverage on layer2'                | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '5', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
+        'one record, cloud type code on layer2'         | [['44', '1979010100', '1', '2', 'P', null, null, null, null, '1', null, null, null, null, null, null, null, null, null, null, null]]
+        'one record, cloud type abbreviation on layer2' | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, 'CI', null, null, null, null, null, null, null, null, null, null]]
+        'one record, cloud height on layer2'            | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, '1234', null, null, null, null, null, null, null, null, null]]
+        'one record, coverage on layer2'                | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, '5', null, null, null, null, null, null, null, null]]
 
     }
 
@@ -138,14 +121,7 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         when: 'the input is converted'
         def records = converter.convert(ssvData).collect(Collectors.toList())
 
-        then: 'each input line has a corresponding cloud type record'
-        records*.stationId == values.collect { StationId.of(it[0]) }
-        records*.measurementTime == values.collect { LocalDateTime.parse(it[1], DateTimeFormatter.ofPattern('yyyyMMddHH')) }
-        records*.qualityLevel == values.collect { QualityLevel.of(it[2]) }
-        records*.overallCoverage == values.collect { Integer.parseInt(it[3]) }
-        records*.measurementOrObservation == values.collect { MeasurementOrObservation.of(it[4]) }
-
-        and: 'only the third layer is set'
+        then: 'only the third layer is set'
         records.every { it.layer1 == null }
         records.every { it.layer2 == null }
         records.every { it.layer3 != null }
@@ -153,15 +129,15 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
 
         and: 'the values in the third layer are set correctly'
         records*.layer3.collect { it.cloudType } == values.collect { cloudTypeOfCodeAndAbbreviation(it[13], it[14]) }
-        records*.layer3.collect { it.height } == values.collect { it[15] == '-999' ? null : Integer.parseInt(it[15]) }
-        records*.layer3.collect { it.coverage } == values.collect { it[16] == '-999' ? null : Integer.parseInt(it[16]) }
+        records*.layer3.collect { it.height } == values.collect { it[15] == null ? null : Integer.parseInt(it[15]) }
+        records*.layer3.collect { it.coverage } == values.collect { it[16] == null ? null : Integer.parseInt(it[16]) }
 
         where:
         description                                     | values
-        'one record, cloud type code on layer3'         | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '1', '-999', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud type abbreviation on layer3' | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', 'CI', '-999', '-999', '-999', '-999', '-999', '-999']]
-        'one record, cloud height on layer3'            | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '1234', '-999', '-999', '-999', '-999', '-999']]
-        'one record, coverage on layer3'                | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '5', '-999', '-999', '-999', '-999']]
+        'one record, cloud type code on layer3'         | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, '1', null, null, null, null, null, null, null]]
+        'one record, cloud type abbreviation on layer3' | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, 'CI', null, null, null, null, null, null]]
+        'one record, cloud height on layer3'            | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, '1234', null, null, null, null, null]]
+        'one record, coverage on layer3'                | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, '5', null, null, null, null]]
     }
 
     @Unroll('#description')
@@ -175,14 +151,7 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
         when: 'the input is converted'
         def records = converter.convert(ssvData).collect(Collectors.toList())
 
-        then: 'each input line has a corresponding cloud type record'
-        records*.stationId == values.collect { StationId.of(it[0]) }
-        records*.measurementTime == values.collect { LocalDateTime.parse(it[1], DateTimeFormatter.ofPattern('yyyyMMddHH')) }
-        records*.qualityLevel == values.collect { QualityLevel.of(it[2]) }
-        records*.overallCoverage == values.collect { Integer.parseInt(it[3]) }
-        records*.measurementOrObservation == values.collect { MeasurementOrObservation.of(it[4]) }
-
-        and: 'only the fourth layer is set'
+        then: 'only the fourth layer is set'
         records.every { it.layer1 == null }
         records.every { it.layer2 == null }
         records.every { it.layer3 == null }
@@ -190,19 +159,19 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
 
         and: 'the values in the fourth layer are set correctly'
         records*.layer4.collect { it.cloudType } == values.collect { cloudTypeOfCodeAndAbbreviation(it[17], it[18]) }
-        records*.layer4.collect { it.height } == values.collect { it[19] == '-999' ? null : Integer.parseInt(it[19]) }
-        records*.layer4.collect { it.coverage } == values.collect { it[20] == '-999' ? null : Integer.parseInt(it[20]) }
+        records*.layer4.collect { it.height } == values.collect { it[19] == null ? null : Integer.parseInt(it[19]) }
+        records*.layer4.collect { it.coverage } == values.collect { it[20] == null ? null : Integer.parseInt(it[20]) }
 
         where:
         description                                     | values
-        'one record, cloud type code on layer4'         | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '1', '-999', '-999', '-999']]
-        'one record, cloud type abbreviation on layer4' | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', 'CI', '-999', '-999']]
-        'one record, cloud height on layer4'            | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '1234', '-999']]
-        'one record, coverage on layer4'                | [['44', '1979010100', '1', '2', 'P', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '-999', '5']]
+        'one record, cloud type code on layer4'         | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, null, '1', null, null, null]]
+        'one record, cloud type abbreviation on layer4' | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, null, null, 'CI', null, null]]
+        'one record, cloud height on layer4'            | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, null, null, null, '1234', null]]
+        'one record, coverage on layer4'                | [['44', '1979010100', '1', '2', 'P', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '5']]
     }
 
     static CloudType cloudTypeOfCodeAndAbbreviation(String code, String abbreviation) {
-        if (code != NULL_STRING && abbreviation != NULL_STRING) {
+        if (code != null && abbreviation != null) {
             CloudType byCode = CloudType.of(code)
             CloudType byAbbreviation = CloudType.ofAbbreviation(abbreviation)
             if (byCode != byAbbreviation) {
@@ -210,9 +179,9 @@ class SsvToHourlyCloudTypeRecordConverterSpec extends Specification {
             } else {
                 return byCode
             }
-        } else if (code != NULL_STRING) {
+        } else if (code != null) {
             return CloudType.of(code)
-        } else if (abbreviation != NULL_STRING) {
+        } else if (abbreviation != null) {
             return CloudType.ofAbbreviation(abbreviation)
         }
         return null
