04f34636fe253c2e65f53ec2be7b095fdc7e3641
kai <kontakt@kaidrahmann.de>
2021-11-13 19:20:48 +0100

    add test for verifying behaviour for invalid input

--- src/test/groovy/rootheart/codes/weatherhistory/importer/SsvToHourlyAirTemperatureRecordConverterSpec.groovy
+++ src/test/groovy/rootheart/codes/weatherhistory/importer/SsvToHourlyAirTemperatureRecordConverterSpec.groovy
@@ -1,5 +1,6 @@
 package rootheart.codes.weatherhistory.importer
 
+import rootheart.codes.weatherhistory.importer.converter.InvalidColumnsException
 import rootheart.codes.weatherhistory.importer.converter.SsvToHourlyAirTemperatureRecordConverter
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -33,7 +34,29 @@ class SsvToHourlyAirTemperatureRecordConverterSpec extends Specification {
 
         and:
         description   | values
+        'no records'  | []
         'one record'  | [['1', '2021110901', '1', '19.3', '70.5']]
         'two records' | [['4711', '2001010100', '8', '-3.6', '-70.5'], ['4711', '2020020200', '5', '7.6', '12.3']]
+    }
+
+    def 'Test that an error is thrown if the column names are not as expected'() {
+        given: 'a converter able to convert from semicolon-separated data to hourly air temperature records'
+        def converter = SsvToHourlyAirTemperatureRecordConverter.INSTANCE
+
+        and: 'some data from a semicolon-separated file'
+        def ssvData = new SsvData(columnNames, [].stream())
+
+        when: 'the input is converted'
+        converter.convert(ssvData).collect(Collectors.toList())
+
+        then:
+        thrown expectedException
+
+        where:
+        columnNames                                                                                | expectedException
+        ["STATIONS_ID", "MESS_DATUM", "QN_9", "TT_TU", "RF_TU", "one column that is not expected"] | InvalidColumnsException
+        ["STATIONS_ID", "MESS_DATUM", "QN_9", "TT_TU", "RF_TU is missing here"]                    | InvalidColumnsException
+        []                                                                                         | InvalidColumnsException
+//        null                                                                                       | NullPointerException
     }
 }
