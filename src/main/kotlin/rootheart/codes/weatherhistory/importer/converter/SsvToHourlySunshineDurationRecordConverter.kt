b042cdd962cd972e9e036881321a5171760638b2
kai <kontakt@kaidrahmann.de>
2021-11-14 15:01:33 +0100

    add converter for hourly sunshine duration records

---
+++ src/main/kotlin/rootheart/codes/weatherhistory/importer/converter/SsvToHourlySunshineDurationRecordConverter.kt
@@ -0,0 +1,11 @@
+package rootheart.codes.weatherhistory.importer.converter
+
+import rootheart.codes.weatherhistory.importer.records.HourlySunshineDurationRecord
+
+object SsvToHourlySunshineDurationRecordConverter : RecordConverter<HourlySunshineDurationRecord>(
+    ::HourlySunshineDurationRecord,
+    mapOf(
+        "QN_7" to QualityLevelProperty(HourlySunshineDurationRecord::qualityLevel),
+        "SD_SO" to BigDecimalProperty(HourlySunshineDurationRecord::sunshineDuration)
+    )
+)
--- src/main/kotlin/rootheart/codes/weatherhistory/importer/records/HourlySunshineDurationRecord.kt
+++ src/main/kotlin/rootheart/codes/weatherhistory/importer/records/HourlySunshineDurationRecord.kt
@@ -1,13 +1,5 @@
 package rootheart.codes.weatherhistory.importer.records
 
-import rootheart.codes.weatherhistory.importer.QualityLevel
-import rootheart.codes.weatherhistory.importer.StationId
 import java.math.BigDecimal
-import java.time.LocalDateTime
 
-data class HourlySunshineDurationRecord(
-    val stationId: StationId,
-    val measurementTime: LocalDateTime,
-    val qualityLevel: QualityLevel,
-    val sunshineDuration: BigDecimal
-)
+data class HourlySunshineDurationRecord(var sunshineDuration: BigDecimal? = null) : BaseRecord()
