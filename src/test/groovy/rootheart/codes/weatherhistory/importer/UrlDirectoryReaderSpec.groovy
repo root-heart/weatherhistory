44d5e2e8c194745d93fcd9a0f14433f0a71657ed
kai <kontakt@kaidrahmann.de>
2021-12-27 17:06:15 +0100

    refactor test of UrlDirectoryReader

--- src/test/groovy/rootheart/codes/weatherhistory/importer/UrlDirectoryReaderSpec.groovy
+++ src/test/groovy/rootheart/codes/weatherhistory/importer/UrlDirectoryReaderSpec.groovy
@@ -24,62 +24,46 @@ class UrlDirectoryReaderSpec extends Specification {
 
     def 'Test that processing an index HTML file from DWD works'() {
         given: "A mocked web server"
-        ClientAndServer.startClientAndServer(8383)
-        def mockServer = new MockServerClient("127.0.0.1", 8383)
+        def randomPort = random.nextInt(10000) + 8000
+        ClientAndServer.startClientAndServer(randomPort)
+        def mockServer = new MockServerClient("127.0.0.1", randomPort)
 
         and: "That serves a directory listing containing links to a station data file and a data file with randomly generated names"
         def stationDataFileName = Gen.string(stationFilenamePattern).first()
         def dataFileName = Gen.string(dataFilenamePattern).first()
         def directoryListing = directoryListing(stationDataFileName, dataFileName)
-        def response = new HttpResponse()
-                .withStatusCode(200)
-                .withBody(directoryListing)
-        mockServer.when(request("GET", "/"))
-                .respond(response)
+        mockServer
+                .when(request("GET", "/"))
+                .respond(respond(200, directoryListing))
 
         and: "That serves the station data file for the station data file name"
-        def stationDataFileContent = stationDataFileContent()
-        response = new HttpResponse()
-                .withStatusCode(200)
-                .withBody(stationDataFileContent.getBytes())
         mockServer.when(request("GET", "/$stationDataFileName"))
-                .respond(response)
+                .respond(respond(200, stationDataFileContent()))
 
         and: "That serves the zipped data file for the data file name"
         def dataFileContent = dataFileContent()
         def baos = new ByteArrayOutputStream()
-        def entry = new ZipEntry("produkt_what_comes_next_doesnt_matter.txt")
+        def entry = new ZipEntry("produkt_what_comes_here_doesnt_matter.txt")
         new ZipOutputStream(baos).with {
             putNextEntry(entry)
             write(dataFileContent.getBytes())
             close()
         }
-        response = new HttpResponse()
-                .withStatusCode(200)
-                .withBody(baos.toByteArray())
         mockServer.when(request("GET", "/$dataFileName"))
-                .respond(response)
+                .respond(respond(200, baos.toByteArray()))
 
-        and:
-        RecordProperty<TestRecord> property1 = { TestRecord record, String value ->
-            record.setProperty1(value)
-        }
-        RecordProperty<TestRecord> property2 = { TestRecord record, String value ->
-            record.setProperty2(value)
-        }
-        RecordProperty<TestRecord> property3 = { TestRecord record, String value ->
-            record.setProperty3(value)
-        }
-        def columnMapping = ["column1": property1, "column2": property2, "column3": property3]
-        def constructor = TestRecord::new
-        def recordConverter = new RecordConverter(constructor, columnMapping, 0, 0)
+        and: "A converter able to populate TestRecords with data"
+        def columnMapping = [
+                "column1": { TestRecord r, String v -> r.property1 = v } as RecordProperty<TestRecord>,
+                "column2": { TestRecord r, String v -> r.property2 = v } as RecordProperty<TestRecord>,
+                "column3": { TestRecord r, String v -> r.property3 = v } as RecordProperty<TestRecord>
+        ]
+        def recordConverter = new RecordConverter(TestRecord::new, columnMapping, 0, 0)
 
         when: "The data is downloaded from the server and parsed"
-        def reader = new UrlDirectoryReader(new URL("http://127.0.0.1:8383"))
-
-        def recordsStream = reader
+        def records = new UrlDirectoryReader(new URL("http://127.0.0.1:$randomPort"))
                 .createStreamForDownloadingAndConvertingZippedDataFiles(recordConverter)
-        def records = recordsStream.collect(Collectors.toList())
+                .collect(Collectors.toList())
 
         then: "The downloaded data equals the data specified before"
         records != null
@@ -122,7 +106,7 @@ class UrlDirectoryReaderSpec extends Specification {
         ].join('\n')
     }
 
-    private static String dataFileContent() {
+    private static String dataFileContent(/*int stationId, String[] measurementDates, String[] values1, String[] values2, String[] values3*/) {
         return [
                 "STATIONS_ID;MESS_DATUM;column1;column2;column3;eor",
                 "3;1979110421;    1;    6.3;    3.0;eor"
@@ -131,8 +115,20 @@ class UrlDirectoryReaderSpec extends Specification {
 
     private static request(String method, String path) {
         new HttpRequest()
-                .withMethod("GET")
+                .withMethod(method)
                 .withPath(path)
+    }
+
+    private static respond(int status, byte[] body) {
+        new HttpResponse()
+                .withStatusCode(status)
+                .withBody(body)
+    }
+
+    private static respond(int status, String body) {
+        new HttpResponse()
+                .withStatusCode(status)
+                .withBody(body)
     }
 
     private static String fileLink(String target) {
