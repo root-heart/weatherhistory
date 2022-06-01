22483bd573945a9779f62237970a53ee04dfa357
Kai Drahmann <kai.drahmann@users.noreply.github.com>
2022-06-01 11:07:23 +0200

    introduce ConcurrentExecutor to better allow concurrent downloading and processing of data

--- importer/src/main/kotlin/rootheart/codes/weatherhistory/importer/MeasurementsImporter.kt
+++ importer/src/main/kotlin/rootheart/codes/weatherhistory/importer/MeasurementsImporter.kt
@@ -1,13 +1,11 @@
 package rootheart.codes.weatherhistory.importer
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
+import rootheart.codes.common.ConcurrentExecutor
 import rootheart.codes.common.strings.splitAndTrimTokens
 import rootheart.codes.weatherhistory.database.DateInterval
 import rootheart.codes.weatherhistory.database.HourlyMeasurement
@@ -25,11 +23,11 @@ import kotlin.system.measureTimeMillis
 private val log = KotlinLogging.logger {}
 
 @DelicateCoroutinesApi
-private val unzipContext = newFixedThreadPoolContext(40, "unzip")
+private val databaseExecutor = ConcurrentExecutor(40, "database-operations")
 
 @DelicateCoroutinesApi
 fun importMeasurements(rootDirectory: HtmlDirectory) {
-    val stationIds = setOf(/*"00848", "13776", "01993", "04371", "00662", "02014", "00850", "01443",*/ "00691")
+    val stationIds = setOf("00848", "13776", "01993", "04371", "00662", "02014", "00850", "01443", "00691")
     val stationByExternalId = StationDao.findAll().associateBy(Station::externalId)
     val duration = measureTimeMillis {
         runBlocking {
@@ -37,54 +35,59 @@ fun importMeasurements(rootDirectory: HtmlDirectory) {
                 .filter { stationIds.contains(it.externalId) }
                 .groupBy { it.externalId }
                 .mapKeys { stationByExternalId[it.key]!! }
-                .forEach(::convertAndImport)
+                .map { MeasurementsImporter(it.key, it.value) }
+                .forEach { it.downloadAndConvert() }
+            log.info { "Waiting for database jobs to complete ..." }
+            databaseExecutor.awaitCompletion()
         }
     }
     log.info { "Finished import in $duration milliseconds, exiting program" }
 }
 
 @DelicateCoroutinesApi
-private fun CoroutineScope.convertAndImport(station: Station, zippedDataFiles: List<ZippedDataFile>) {
-    val xyz = Xyz(station)
-    val measurements = xyz.downloadAndConvert(zippedDataFiles)
-    log.info { "${station.id} - Converted: ${measurements.size}" }
-
-    launch { HourlyMeasurementsImporter.importEntities(measurements) }
-
-    launch {
-        val summarizedMeasurements = xyz.summarizeMeasurements(measurements)
-        SummarizedMeasurementImporter.importEntities(summarizedMeasurements)
-    }
-
-    log.info { "Station ${station.id} - Converted and saved: ${measurements.size}" }
-}
-
-@DelicateCoroutinesApi
-private class Xyz(val station: Station) {
+private class MeasurementsImporter(val station: Station, val zippedDataFiles: Collection<ZippedDataFile>) {
     private val measurementByTime = ConcurrentHashMap<DateTime, HourlyMeasurement>()
+    private val unzipParseConvertExecutor = ConcurrentExecutor(40, "unzip")
 
     @DelicateCoroutinesApi
-    fun downloadAndConvert(zippedDataFiles: Collection<ZippedDataFile>): Collection<HourlyMeasurement> =
+    suspend fun downloadAndConvert() {
         runBlocking {
             for (zippedDataFile in zippedDataFiles.sortedByDescending { it.size }) {
                 log.info { "Station ${station.id}, file ${zippedDataFile.fileName} - Downloading" }
                 val zippedBytes = zippedDataFile.url.readBytes()
                 log.info { "Station ${station.id}, file ${zippedDataFile.fileName} - Downloaded" }
-
-                unzipAndConvert(zippedDataFile, zippedBytes)
+                unzipParseConvertExecutor.run { unzipAndConvert(zippedDataFile, zippedBytes) }
             }
-            log.info { "Station ${station.id} - Waiting for unzip-parse-converters to finish their job" }
-            return@runBlocking measurementByTime.values
         }
+        log.info { "Station ${station.id} - Waiting for unzip-parse-convert jobs to complete" }
+        unzipParseConvertExecutor.awaitCompletion()
 
-    private fun CoroutineScope.unzipAndConvert(zippedDataFile: ZippedDataFile, zippedBytes: ByteArray) =
-        launch(unzipContext) {
-            val duration = measureTimeMillis {
-                unzip(zippedBytes)
-                    .let(::parse)
-                    .let { convert(it, zippedDataFile.measurementType.columnNameMapping) }
+        databaseExecutor.run { HourlyMeasurementsImporter.importEntities(measurementByTime.values) }
+
+        databaseExecutor.run {
+            val summarizedMeasurements = summarizeMeasurements()
+            SummarizedMeasurementImporter.importEntities(summarizedMeasurements)
+            log.info { "Station ${station.id} - Converted and saved: ${measurementByTime.size}" }
+        }
+    }
+
+    private fun unzipAndConvert(zippedDataFile: ZippedDataFile, zippedBytes: ByteArray) {
+        val duration = measureTimeMillis {
+            val unzippedBytes = unzip(zippedBytes)
+            val parsed = parse(unzippedBytes)
+            convert(parsed, zippedDataFile.measurementType.columnNameMapping)
+        }
+        log.info { "Station ${station.id}, file ${zippedDataFile.fileName} - converting ${measurementByTime.size} took $duration millis" }
+    }
+
+    private fun unzip(zippedBytes: ByteArray): ByteArray =
+        ZipInputStream(ByteArrayInputStream(zippedBytes)).use { zipInputStream ->
+            val entries = generateSequence { zipInputStream.nextEntry }
+            if (entries.any { fileIsMeasurementFile(it.name) }) {
+                return@use zipInputStream.readBytes()
+            } else {
+                return@use ByteArray(0)
             }
-            log.info { "Station ${station.id}, file ${zippedDataFile.fileName} - converting took $duration millis" }
         }
 
     fun parse(bytes: ByteArray): SemicolonSeparatedValues =
@@ -95,7 +98,10 @@ private class Xyz(val station: Station) {
             return SemicolonSeparatedValues(columnNames, columnValues)
         }
 
-    private fun convert(semicolonSeparatedValues: SemicolonSeparatedValues, columnNameMapping: Map<String, SimpleMeasurementProperty<*>>) {
+    private fun convert(
+        semicolonSeparatedValues: SemicolonSeparatedValues,
+        columnNameMapping: Map<String, SimpleMeasurementProperty<*>>
+    ) {
         val indexMeasurementTime =
             semicolonSeparatedValues.columnNames.indexOf(COLUMN_NAME_MEASUREMENT_TIME)
         val columnMappingByIndex = columnNameMapping.mapKeys {
@@ -117,8 +123,9 @@ private class Xyz(val station: Station) {
     }
 
     @DelicateCoroutinesApi
-    fun summarizeMeasurements(measurements: Collection<HourlyMeasurement>): List<SummarizedMeasurement> {
+    fun summarizeMeasurements(): List<SummarizedMeasurement> {
         val summarizedMeasurements = ArrayList<SummarizedMeasurement>()
+        val measurements = measurementByTime.values
         val duration = measureTimeMillis {
             val groupedByDay = measurements.groupBy { DateInterval.day(it.measurementTime) }
             val summarizedByDay = groupedByDay.map { (day, measurements) ->
@@ -148,16 +155,6 @@ private class Xyz(val station: Station) {
         log.info { "Summarizing measurements done in $duration millis" }
         return summarizedMeasurements
     }
-
-    private fun unzip(zippedBytes: ByteArray): ByteArray = ZipInputStream(ByteArrayInputStream(zippedBytes))
-        .use { zipInputStream ->
-            val entries = generateSequence { zipInputStream.nextEntry }
-            if (entries.any { fileIsMeasurementFile(it.name) }) {
-                return@use zipInputStream.readBytes()
-            } else {
-                return@use ByteArray(0)
-            }
-        }
 
     private fun fileIsMeasurementFile(filename: String) =
         filename.startsWith("produkt_") && filename.endsWith(".txt")
