bd6e83d4aad39687913aed5722d0b48b23a70585
Kai Drahmann <kai.drahmann@users.noreply.github.com>
2022-09-09 21:42:49 +0200

    improve concurrency of importer

--- importer/src/main/kotlin/rootheart/codes/weatherhistory/importer/StationsImporter.kt
+++ importer/src/main/kotlin/rootheart/codes/weatherhistory/importer/StationsImporter.kt
@@ -10,7 +10,7 @@ import java.nio.charset.Charset
 private val log = KotlinLogging.logger {}
 
 private val stationFilter: (Station) -> Boolean = {
-    it.hasRecentData && it.hasTemperatureData && it.hasSunshineData && it.hasCloudinessData && it.hasAirPressureData
+    it.hasRecentData && it.hasTemperatureData && it.hasSunshineData && it.hasCloudinessData && it.hasAirPressureData && it.externalId <= "01000"
 }
 
 @DelicateCoroutinesApi
@@ -51,7 +51,7 @@ fun importStations(rootDirectory: HtmlDirectory) {
                     MeasurementType.WIND_SPEED -> station.hasWindData = true
                     MeasurementType.PRECIPITATION -> station.hasPrecipitationData = true
                 }
-                station.hasRecentData = lastDay.startsWith("202208")
+                station.hasRecentData = lastDay.startsWith("202209")
             }
         }
     }
