16e113588740953032f4617c0fbff9878e858320
kai <kontakt@kaidrahmann.de>
2022-12-22 18:08:29 +0100

    further fine-tune displaying of charts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 import {CloudinessChart} from "./charts/cloudiness-chart/cloudiness-chart.component";
+import {Duration} from "luxon";
 
 export type MeasurementTypes = "temperature" | "humidity" | "airPressure" | "visibility"
 
@@ -16,10 +17,25 @@ export class AppComponent {
 
     measurementType?: MeasurementTypes
 
-    columnCount: number = 1
+    columnCount: number = 3
 
     showDetails(measurementType: MeasurementTypes) {
         this.measurementType = measurementType
+    }
+
+    divideBy60(x?: number): number | undefined {
+        return x ? x / 60 : undefined
+    }
+
+    sumOfCloudCoverage(coverageHistogram: number[] | undefined, coverageIndices: number[]): number {
+        if (!coverageHistogram) {
+            return 0
+        }
+        let sum = 0
+        for (let i = 0; i < coverageIndices.length; i++) {
+            sum += coverageHistogram[coverageIndices[i]]
+        }
+        return sum
     }
 }
 
