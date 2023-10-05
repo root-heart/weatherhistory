7cc2a99997e1069c4b7454c0d0fe1fdcbe7dbae1
root-heart <kai.drahmann@users.noreply.github.com>
2023-10-05 02:04:34 +0200

    add sunshine duration and cloud coverage charts

--- frontend/src/app/station-charts/station-charts.component.ts
+++ frontend/src/app/station-charts/station-charts.component.ts
@@ -30,16 +30,43 @@ export class StationChartsComponent implements OnInit {
     data = [1, 2, 3, 4];
 
     Highcharts: typeof Highcharts = Highcharts;
+
     temperatureChart?: Highcharts.Chart;
+    sunshineDurationChart?: Highcharts.Chart;
+    cloudinessChart?: Highcharts.Chart;
+
     chartCallback: Highcharts.ChartCallbackFunction;
+    sunshineDurationChartCallback: Highcharts.ChartCallbackFunction;
+    cloudinessChartCallback: Highcharts.ChartCallbackFunction;
 
     temperatureChartOptions: Highcharts.Options = {
         chart: {styledMode: true, animation: false},
-        yAxis: [{title: {text: 'Air Temperature'}}],
+        title: {text: undefined},
+        yAxis: [{title: {text: undefined}}],
         tooltip: {shared: true},
         plotOptions: {
             line: {animation: false},
             arearange: {animation: false}
+        }
+    }
+
+    sunshineDurationChartOptions: Highcharts.Options = {
+        chart: {styledMode: true, animation: false},
+        title: {text: undefined},
+        yAxis: [{title: {text: undefined}}],
+        tooltip: {shared: true},
+        plotOptions: {
+            column: {animation: false}
+        }
+    }
+
+    cloudinessChartOptions: Highcharts.Options = {
+        chart: {styledMode: true, animation: false},
+        title: {text: undefined},
+        yAxis: [{title: {text: undefined}, reversed: true}],
+        tooltip: {shared: true},
+        plotOptions: {
+            column: {animation: false}
         }
     }
 
@@ -54,32 +81,77 @@ export class StationChartsComponent implements OnInit {
     measurementType?: MeasurementTypes
 
     constructor(public filterService: FilterService) {
-        this.chartCallback = (c: any) => {
-            this.temperatureChart = c;
-        };
+        this.chartCallback = c => this.temperatureChart = c;
+        this.sunshineDurationChartCallback = c => this.sunshineDurationChart = c
+        this.cloudinessChartCallback = c => this.cloudinessChart = c
 
         filterService.currentData.subscribe(data => {
-            if (data && this.temperatureChart) {
-                let airTemps = data.details
-                    .map(m => m.measurements)
-                    .filter(m => m)
-                    .map(m => m!.airTemperatureCentigrade)
-                    .filter(t => t)
-                let minMax = airTemps
-                    .map(t => [t!.min, t!.max]);
-                let avg = airTemps.map(t => t.avg)
-                while (this.temperatureChart.series.length > 0) {
-                    this.temperatureChart.series[0].remove()
+            if (data) {
+                if (this.temperatureChart) {
+                    let airTemps = data.details
+                        .map(m => m.measurements)
+                        .filter(m => m)
+                        .map(m => m!.airTemperatureCentigrade)
+                        .filter(t => t)
+                    let minMax = airTemps
+                        .map(t => [t!.min, t!.max]);
+                    let avg = airTemps.map(t => t.avg)
+                    while (this.temperatureChart.series.length > 0) {
+                        this.temperatureChart.series[0].remove()
+                    }
+                    this.temperatureChart.addSeries({
+                        type: 'arearange',
+                        data: minMax,
+                        lineWidth: 3,
+                        marker: { enabled: false },
+                        states: { hover: { enabled: false } }
+                    })
+                    this.temperatureChart.addSeries({
+                        type: 'line',
+                        data: avg,
+                        marker: { enabled: false }
+                    })
                 }
-                this.temperatureChart.addSeries({
-                    type: 'arearange',
-                    data: minMax,
-                    lineWidth: 3
-                })
-                this.temperatureChart.addSeries({
-                    type: 'line',
-                    data: avg
-                })
+
+                if (this.sunshineDurationChart) {
+                    let sunshineDurations = data.details.map(m => m.measurements)
+                        .filter(m => m)
+                        .map(m => m!.sunshineMinutes)
+                        .filter(s => s)
+                    while (this.sunshineDurationChart.series.length > 0) {
+                        this.sunshineDurationChart.series[0].remove()
+                    }
+                    this.sunshineDurationChart.addSeries({
+                        type: "column",
+                        data: sunshineDurations.map(s => s.sum! / 60),
+                        pointPadding: 0,
+                        groupPadding: 0,
+                        borderRadius: 0,
+                        borderWidth: 0
+                    })
+                }
+
+                if (this.cloudinessChart) {
+                    let cloudiness = data.details.map(m => m.measurements)
+                        .filter(m => m)
+                        .map(m => m?.cloudCoverageHistogram)
+                        .filter(h => h)
+                    while (this.cloudinessChart.series.length > 0) {
+                        this.cloudinessChart.series[0].remove()
+                    }
+                    for (let i = 0; i <= 8; i++) {
+                        this.cloudinessChart.addSeries({
+                            type: "column",
+                            data: cloudiness.map(c => c![i]),
+                            stack: 's',
+                            stacking: 'percent',
+                            pointPadding: 0,
+                            groupPadding: 0,
+                            borderRadius: 0,
+                            borderWidth: 0
+                        })
+                    }
+                }
             }
         })
     }
