7467a61833aeb4eb67186eabc10b9ea083195257
root-heart <kai.drahmann@users.noreply.github.com>
2023-10-05 20:34:01 +0200

    add precipitation chart and improve x-axis date handling

--- frontend/src/app/charts/charts.ts
+++ frontend/src/app/charts/charts.ts
@@ -1,14 +1,27 @@
 import {DateTime} from "luxon";
 import {DailyMeasurement, MonthlySummary, YearlySummary} from "../data-classes";
 
-export function getDateLabel(m: DailyMeasurement | MonthlySummary | YearlySummary): string {
-  // console.log(m)
-  if ("date" in m) {
-    return DateTime.fromFormat(m.date!, "yyyy-MM-dd").toFormat("dd.MM.")
-  } else if ("month" in m && "year" in m) {
-    return DateTime.fromObject({year: m.year, month: m.month}).toFormat("MMMM yyyy")
-  } else if ("year" in m) {
-    return DateTime.fromObject({year: m.year}).toFormat("yyyy")
-  }
-  return DateTime.fromObject({year: 1979, month: 11, day: 11}).toFormat("dd.MM.yyyy")
+// export function getDateLabel(m: DailyMeasurement | MonthlySummary | YearlySummary): string {
+//   // console.log(m)
+//   if ("date" in m) {
+//     return DateTime.fromFormat(m.date!, "yyyy-MM-dd").toFormat("dd.MM.")
+//   } else if ("month" in m && "year" in m) {
+//     return DateTime.fromObject({year: m.year, month: m.month}).toFormat("MMMM yyyy")
+//   } else if ("year" in m) {
+//     return DateTime.fromObject({year: m.year}).toFormat("yyyy")
+//   }
+//   return DateTime.fromObject({year: 1979, month: 11, day: 4}).toFormat("dd.MM.yyyy")
+// }
+
+
+export function getDateLabel(m: DailyMeasurement | MonthlySummary | YearlySummary): number {
+    // console.log(m)
+    if ("date" in m) {
+        return DateTime.fromFormat(m.date!, "yyyy-MM-dd", {zone: "UTC"}).toJSDate().getTime()
+    } else if ("month" in m && "year" in m) {
+        return DateTime.fromObject({year: m.year, month: m.month}).toJSDate().getTime()
+    } else if ("year" in m) {
+        return DateTime.fromObject({year: m.year}).toJSDate().getTime()
+    }
+    return DateTime.fromObject({year: 1979, month: 11, day: 4}).toJSDate().getTime()
 }
--- frontend/src/app/station-charts/station-charts.component.ts
+++ frontend/src/app/station-charts/station-charts.component.ts
@@ -14,8 +14,13 @@ import {
 import * as Highcharts from 'highcharts';
 
 import addMore from "highcharts/highcharts-more";
+import {getDateLabel} from "../charts/charts";
+import {formatDate, registerLocaleData} from "@angular/common";
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
 
 addMore(Highcharts);
+registerLocaleData(localeDe, 'de-DE', localeDeExtra);
 
 @Component({
     selector: 'station-charts',
@@ -34,39 +39,32 @@ export class StationChartsComponent implements OnInit {
     temperatureChart?: Highcharts.Chart;
     sunshineDurationChart?: Highcharts.Chart;
     cloudinessChart?: Highcharts.Chart;
+    precipitationChart?: Highcharts.Chart;
 
     chartCallback: Highcharts.ChartCallbackFunction;
     sunshineDurationChartCallback: Highcharts.ChartCallbackFunction;
     cloudinessChartCallback: Highcharts.ChartCallbackFunction;
+    precipitationChartCallback: Highcharts.ChartCallbackFunction;
 
-    temperatureChartOptions: Highcharts.Options = {
-        chart: {styledMode: true, animation: false},
+    chartOptions: Highcharts.Options = {
+        chart: {styledMode: true, animation: false, zooming: {mouseWheel: {enabled: true}, type: "x"}},
         title: {text: undefined},
-        yAxis: [{title: {text: undefined}}],
-        tooltip: {shared: true},
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                formatter: v => new Date(v.value).toLocaleDateString('de-DE', {month: "short"})
+            },
+        },
+        yAxis: [{title: {text: undefined}, reversedStacks: false}],
+        tooltip: {
+            shared: true,
+            xDateFormat: "%d.%m.%Y"
+        },
         plotOptions: {
             line: {animation: false},
-            arearange: {animation: false}
-        }
-    }
-
-    sunshineDurationChartOptions: Highcharts.Options = {
-        chart: {styledMode: true, animation: false},
-        title: {text: undefined},
-        yAxis: [{title: {text: undefined}}],
-        tooltip: {shared: true},
-        plotOptions: {
+            arearange: {animation: false},
             column: {animation: false}
-        }
-    }
 
-    cloudinessChartOptions: Highcharts.Options = {
-        chart: {styledMode: true, animation: false},
-        title: {text: undefined},
-        yAxis: [{title: {text: undefined}, reversed: true}],
-        tooltip: {shared: true},
-        plotOptions: {
-            column: {animation: false}
         }
     }
 
@@ -84,71 +82,96 @@ export class StationChartsComponent implements OnInit {
         this.chartCallback = c => this.temperatureChart = c;
         this.sunshineDurationChartCallback = c => this.sunshineDurationChart = c
         this.cloudinessChartCallback = c => this.cloudinessChart = c
+        this.precipitationChartCallback = c => this.precipitationChart = c
 
         filterService.currentData.subscribe(data => {
             if (data) {
                 if (this.temperatureChart) {
                     let airTemps = data.details
-                        .map(m => m.measurements)
-                        .filter(m => m)
-                        .map(m => m!.airTemperatureCentigrade)
-                        .filter(t => t)
-                    let minMax = airTemps
-                        .map(t => [t!.min, t!.max]);
-                    let avg = airTemps.map(t => t.avg)
+                        .map(m => (
+                            {date: getDateLabel(m), temps: m.measurements?.airTemperatureCentigrade}
+                        ));
                     while (this.temperatureChart.series.length > 0) {
                         this.temperatureChart.series[0].remove()
                     }
                     this.temperatureChart.addSeries({
                         type: 'arearange',
-                        data: minMax,
+                        data: airTemps.map(t =>
+                            [t.date, t.temps?.min, t.temps?.max]
+                        ),
                         lineWidth: 3,
-                        marker: { enabled: false },
-                        states: { hover: { enabled: false } }
+                        marker: {enabled: false},
+                        states: {hover: {enabled: false}}
                     })
                     this.temperatureChart.addSeries({
                         type: 'line',
-                        data: avg,
-                        marker: { enabled: false }
+                        data: airTemps.map(t =>
+                            [t.date, t.temps?.avg]
+                        ),
+                        marker: {enabled: false}
                     })
                 }
 
                 if (this.sunshineDurationChart) {
-                    let sunshineDurations = data.details.map(m => m.measurements)
-                        .filter(m => m)
-                        .map(m => m!.sunshineMinutes)
-                        .filter(s => s)
+                    let sunshineDurations = data.details
+                        .map(m => (
+                            {date: getDateLabel(m), sunshineDurations: m.measurements!.sunshineMinutes}
+                        ));
                     while (this.sunshineDurationChart.series.length > 0) {
                         this.sunshineDurationChart.series[0].remove()
                     }
                     this.sunshineDurationChart.addSeries({
                         type: "column",
-                        data: sunshineDurations.map(s => s.sum! / 60),
-                        pointPadding: 0,
-                        groupPadding: 0,
-                        borderRadius: 0,
-                        borderWidth: 0
+                        data: sunshineDurations.map(s =>
+                            [s.date, s.sunshineDurations.sum! / 60]
+                        ),
+                        borderRadius: 0
+                    })
+                }
+
+                if (this.precipitationChart) {
+                    let sunshineDurations = data.details
+                        .map(m => (
+                            {date: getDateLabel(m), rain: m.measurements!.rainfallMillimeters, snow: m.measurements!.snowfallMillimeters}
+                        ));
+                    while (this.precipitationChart.series.length > 0) {
+                        this.precipitationChart.series[0].remove()
+                    }
+                    this.precipitationChart.addSeries({
+                        type: "column",
+                        data: sunshineDurations.map(s =>
+                            [s.date, s.rain.sum!]
+                        ),
+                        borderRadius: 0
+                    })
+                    this.precipitationChart.addSeries({
+                        type: "column",
+                        data: sunshineDurations.map(s =>
+                            [s.date, s.snow.sum!]
+                        ),
+                        borderRadius: 0
                     })
                 }
 
                 if (this.cloudinessChart) {
-                    let cloudiness = data.details.map(m => m.measurements)
-                        .filter(m => m)
-                        .map(m => m?.cloudCoverageHistogram)
-                        .filter(h => h)
+                    let cloudiness = data.details
+                        .map(m => (
+                            {date: getDateLabel(m), cloudiness: m.measurements?.cloudCoverageHistogram}
+                        ));
                     while (this.cloudinessChart.series.length > 0) {
                         this.cloudinessChart.series[0].remove()
                     }
                     for (let i = 0; i <= 8; i++) {
                         this.cloudinessChart.addSeries({
                             type: "column",
-                            data: cloudiness.map(c => c![i]),
+                            data: cloudiness.map(c =>
+                                [c.date, c.cloudiness![i]]
+                            ),
                             stack: 's',
                             stacking: 'percent',
                             pointPadding: 0,
                             groupPadding: 0,
-                            borderRadius: 0,
-                            borderWidth: 0
+                            borderRadius: 0
                         })
                     }
                 }
