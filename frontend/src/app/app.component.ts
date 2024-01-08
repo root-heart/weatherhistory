b730f73d7bdde584dd9907ca21bb1fa1f3f50bfd
root-heart <kai.drahmann@users.noreply.github.com>
2024-01-08 12:53:36 +0100

    remove unnecessary code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,33 +1,16 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {FilterService} from "./filter.service";
-import {DropdownService} from "./dropdown.service";
+import {Component} from '@angular/core';
 
 export type MeasurementTypes = "temperature" | "humidity" | "airPressure" | "visibility"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  title = 'wetterchroniken.de/';
+export class AppComponent {
+    title = 'wetterchroniken.de/';
 
-  @ViewChild("dropdownBackground") dropdownBackground?: ElementRef
-
-  constructor(public filterService: FilterService, private dropdownService: DropdownService) {
-  }
-
-  ngAfterViewInit() {
-      // TODO remove
-    this.dropdownService.dropdownBackground = this.dropdownBackground
-    setTimeout(() => {
-      // console.log("ngAfterViewInit")
-      // console.log(this.qqq)
-      // console.log(this.temperatureChart)
-      //
-      // this.temperatureChart?.updateChart()
-
-    })
-  }
+    constructor() {
+    }
 }
 
